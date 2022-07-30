f7e3fabf757935f5da0b5db6bcb51024020495d1
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-07-30 15:47:45 -0300

    Initial Commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Vitor Ferraz Varela on 30/07/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
