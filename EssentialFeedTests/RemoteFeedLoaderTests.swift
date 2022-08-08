8f165c3215f3b1f8cfbee1e7ab3b3fb7b6bd9c8a
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-08-08 09:11:23 -0300

    refactor: improve readability with helper method

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -41,7 +41,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
         sut.load { capturedError.append($0) }
         
         let clientError = NSError(domain: "test", code: -1)
-        client.completions[0](clientError)
+        client.complete(with: clientError)
         
         XCTAssertEqual(capturedError, [.connectivity])
     }
@@ -61,6 +61,10 @@ final class RemoteFeedLoaderTests: XCTestCase {
         func get(from url: URL, completion: @escaping (Error) -> Void) {
             requestURLs.append(url)
             completions.append(completion)
+        }
+        
+        func complete(with error: Error, at index: Int = 0) {
+            completions[index](error)
         }
     }
 }
