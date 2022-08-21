f46c86a51e0d3934517505c29680c4e3e6f9a1cc
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-08-21 18:20:17 -0300

    create sut factory method and change to return the protocol instead of the concrete class

--- EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
+++ EssentialFeedTests/FeedAPI/URLSessionHTTPClientTests.swift
@@ -43,7 +43,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
             exp.fulfill()
         }
         
-        URLSessionHTTPClient().get(from: url) { _ in }
+        makeSUT().get(from: url) { _ in }
         wait(for: [exp], timeout: 1.0)
     }
 
@@ -52,10 +52,8 @@ final class URLSessionHTTPClientTests: XCTestCase {
         let error = NSError(domain: "error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
 
-        let sut = URLSessionHTTPClient()
-        
         let exp = expectation(description: "wait for completion")
-        sut.get(from: url) { result in
+        makeSUT().get(from: url) { result in
             switch result {
             case .failure(let receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -69,6 +67,9 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
     
     // MARK: - Helpers
+    private func makeSUT() -> HTTPClient {
+        URLSessionHTTPClient()
+    }
     private class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
         private static var requestObserver: ((URLRequest) -> Void)?
