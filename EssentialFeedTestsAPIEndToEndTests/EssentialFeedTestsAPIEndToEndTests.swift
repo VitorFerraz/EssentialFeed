67c69e449c9445868078ab38bc162f9b8b1a777d
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-08-30 09:07:41 -0300

    test: use ephemeral URL session configuration to avoid in-disk cache

--- EssentialFeedTestsAPIEndToEndTests/EssentialFeedTestsAPIEndToEndTests.swift
+++ EssentialFeedTestsAPIEndToEndTests/EssentialFeedTestsAPIEndToEndTests.swift
@@ -32,7 +32,7 @@ final class EssentialFeedTestsAPIEndToEndTests: XCTestCase {
     // MARK: - Helpers
     private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadFeedResult? {
         let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed")!
-        let client = URLSessionHTTPClient()
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteFeedLoader(url: testServerURL, client: client)
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
