32a19d86b71d1dafbca85f266d38ff8f7adab16b
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-09-09 21:33:44 -0300

    move FeedStore to its own file

---
+++ EssentialFeed/FeedCache/FeedStore.swift
@@ -0,0 +1,15 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Vitor Ferraz Varela on 09/09/22.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], currentDate: Date, completion: @escaping InsertionCompletion)
+}
