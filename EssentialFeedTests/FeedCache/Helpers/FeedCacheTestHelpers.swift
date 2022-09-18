a220e8c3f0cc7c05b9012ba5776dd8cef241c137
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-09-18 18:33:38 -0300

    move feedCacheMaxAgeInDays to a computed var

--- EssentialFeedTests/FeedCache/Helpers/FeedCacheTestHelpers.swift
+++ EssentialFeedTests/FeedCache/Helpers/FeedCacheTestHelpers.swift
@@ -9,11 +9,15 @@ import Foundation
 import EssentialFeed
 
 extension Date {
+    private var feedCacheMaxAgeInDays: Int {
+        7
+    }
+     
     func minusFeedCacheMaxAge() -> Date  {
-        adding(days: -7)
+        adding(days: -feedCacheMaxAgeInDays)
     }
     
-    func adding(days: Int) -> Date {
+    private func adding(days: Int) -> Date {
         Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
     
