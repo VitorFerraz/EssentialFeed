04c982f6d9b69e63471849cb1b54a29526bf448e
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-10-01 17:32:37 -0300

    remove completion nil

--- EssentialFeed/FeedCache/CoreDataFeedStore.swift
+++ EssentialFeed/FeedCache/CoreDataFeedStore.swift
@@ -52,7 +52,6 @@ public final class CoreDataFeedStore: FeedStore {
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        completion(nil)
         let context = self.context
         context.perform {
             do {
