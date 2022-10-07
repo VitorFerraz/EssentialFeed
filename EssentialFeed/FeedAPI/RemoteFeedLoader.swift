6977a22af27a38be867482de3cb23cd74dca5ba3
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-10-07 09:30:41 -0300

    Nest LoadFeedResult into the FeedLoader protocol as FeedLoad.Result since they're closely related

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -10,7 +10,7 @@ import Foundation
 public final class RemoteFeedLoader: FeedLoader {
     private let url: URL
     private let client: HTTPClient
-    public typealias Result = LoadFeedResult
+    public typealias Result = FeedLoader.Result
     
     public enum Error: Swift.Error {
         case connectivity
@@ -22,7 +22,7 @@ public final class RemoteFeedLoader: FeedLoader {
         self.client = client
     }
     
-    public func load(completion: @escaping (LoadFeedResult) -> Void) {
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         client.get(from: url) { [weak self] result in
             guard self != nil else { return }
             switch result {
