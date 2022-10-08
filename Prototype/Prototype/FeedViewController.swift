28153c7957cdac6a438030ca9733034b1e958596
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-10-08 18:18:40 -0300

    add FeedImageCell with data

--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 
+struct FeedImageViewModel {
+    let description: String?
+    let location: String?
+    let imageName: String
+}
+
 class FeedViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return FeedImageViewModel.prototypeFeed.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FeedImageCell") as? FeedImageCell else {
+            return UITableViewCell()
+        }
+        let viewModel = FeedImageViewModel.prototypeFeed[indexPath.row]
+        
+        cell.locationLabel.text = viewModel.location
+        cell.feedImageView.image = UIImage(named: viewModel.imageName)
+        cell.descriptionLabel.text = viewModel.description
+        
+        return cell
     }
 
 }
