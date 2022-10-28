faade82b5c6d386a0c600ca991cd3924e3377ef9
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-10-28 09:46:41 -0300

    extract localized test to a helper function

--- EssentialFeediOSTests/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/FeedViewControllerTests.swift
@@ -16,11 +16,19 @@ final class FeedViewControllerTests: XCTestCase {
         
         sut.loadViewIfNeeded()
         let bundle = Bundle(for: FeedViewController.self)
-        let localizedKey = "FEED_VIEW_TITLE"
-        let localizedTitle = bundle.localizedString(forKey: localizedKey, value: nil, table: "Feed")
-
-        XCTAssertNotEqual(localizedKey, localizedTitle, "Missing localized string for key: \(localizedKey)")
-        XCTAssertEqual(sut.title, localizedTitle)
+ 
+        XCTAssertEqual(sut.title, localized( "FEED_VIEW_TITLE"))
+    }
+    
+    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Feed"
+        let bundle = Bundle(for: FeedViewController.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in  table: \(table)", file: file, line: line)
+        }
+        
+        return value
     }
     
     func test_loadFeedActions_requestFeedFromLoader() {
