df464b758fd97db7fa1932e4b8f8c6ba76254505
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-10-28 09:48:02 -0300

    move localized helper function to its own file

---
+++ EssentialFeediOSTests/FeedViewController+Localization.swift
@@ -0,0 +1,23 @@
+//
+//  FeedViewController+Localization.swift
+//  EssentialFeediOSTests
+//
+//  Created by Vitor Ferraz Varela on 28/10/22.
+//
+
+import Foundation
+import XCTest
+import EssentialFeediOS
+
+extension FeedViewControllerTests {
+    func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Feed"
+        let bundle = Bundle(for: FeedViewController.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in  table: \(table)", file: file, line: line)
+        }
+        
+        return value
+    }
+}
