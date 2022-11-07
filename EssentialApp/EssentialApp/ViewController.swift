67d9148e9fd296a271956988dc4f02d348f27fce
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-11-06 21:08:56 -0300

    Create porjec

---
+++ EssentialApp/EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Vitor Ferraz Varela on 06/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
