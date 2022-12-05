9957f4b52a5e949dfd2a542fb483ac02ebfb4cb9
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-12-04 21:22:49 -0300

    refactor to user map

--- EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderCacheDecoratorTests.swift
@@ -25,10 +25,10 @@ final class FeedLoaderCacheDecorator: FeedLoader {
     
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
         decoratee.load { [weak self] result in
-            if let feed = try? result.get() {
+            completion(result.map{ feed in
                 self?.cache.save(feed) { _ in }
-            }
-            completion(result)
+                return feed
+            })
         }
     }
 }
@@ -58,7 +58,6 @@ final class FeedLoaderCacheDecoratorTests: XCTestCase, FeedLoaderTestCase {
     
     func test_load_doesNotCacheOnLoaderFailure() {
         let cache = CacheSpy()
-        let feed = uniqueFeed()
         let sut = makeSUT(loaderResult: .failure(anyNSError), cache: cache)
         
         sut.load { _ in }
