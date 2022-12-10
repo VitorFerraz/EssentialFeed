3ac965dc4cb7e674ef869c901ee4d8957eed1959
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-12-10 15:45:28 -0300

    create FeedImageDataLoaderDecoratorTests

--- EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
@@ -25,3 +25,20 @@ func uniqueFeed() -> [FeedImage] {
     [FeedImage(id: UUID(), description: "any", location: "any", url: URL(string: "http://any-url.com")!)]
 }
 
+func uniqueImage() -> FeedImage {
+    FeedImage(id: UUID(), url: anyURL)
+}
+
+func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
+    let models = [uniqueImage(), uniqueImage()]
+    let local = models.map {
+        LocalFeedImage(
+            id: $0.id,
+            description: $0.description,
+            location: $0.location,
+            url: $0.url
+        )
+    }
+    return (models, local)
+}
+
