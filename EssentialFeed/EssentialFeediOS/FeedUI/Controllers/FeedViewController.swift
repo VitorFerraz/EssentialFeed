41bf73b034aebb8266520a98fed2a440904eacaa
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-12-23 11:38:48 -0300

    fix potential bug when cancelling requests in UITableView didEndDisplayingCell method - This method is invoke after calling `reloadData`, so we'd be cancelling requests in the wrong models or crash in case the model has less items than the previous model

--- EssentialFeed/EssentialFeediOS/FeedUI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/FeedUI/Controllers/FeedViewController.swift
@@ -11,6 +11,7 @@ public protocol FeedViewControllerDelegate {
 
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView, FeedErrorView {
     public var delegate: FeedViewControllerDelegate?
+    private var loadingControllers: [IndexPath: FeedImageCellController] = [:]
     @IBOutlet private(set) public var errorView: ErrorView?
     
     private var tableModel = [FeedImageCellController]() {
@@ -34,6 +35,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     public func display(_ cellControllers: [FeedImageCellController]) {
+        loadingControllers = [:]
         tableModel = cellControllers
     }
     
@@ -68,10 +70,13 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
     
     private func cellController(forRowAt indexPath: IndexPath) -> FeedImageCellController {
-        return tableModel[indexPath.row]
+        let controller = tableModel[indexPath.row]
+        loadingControllers[indexPath] = controller
+        return controller
     }
     
     private func cancelCellControllerLoad(forRowAt indexPath: IndexPath) {
-        cellController(forRowAt: indexPath).cancelLoad()
+        loadingControllers[indexPath]?.cancelLoad()
+        loadingControllers[indexPath] = nil
     }
 }
