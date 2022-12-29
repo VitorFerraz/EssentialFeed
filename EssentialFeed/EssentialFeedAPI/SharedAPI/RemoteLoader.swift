ede79993b3d020cb5aeb69091e371a63adc3f094
Vitor Ferraz Varela <vitor.varela@bitso.com>
2022-12-29 11:54:39 -0300

    Implement generic RemoteLoader

--- EssentialFeed/EssentialFeedAPI/SharedAPI/RemoteLoader.swift
+++ EssentialFeed/EssentialFeedAPI/SharedAPI/RemoteLoader.swift
@@ -8,36 +8,39 @@
 import Foundation
 import EssentialFeed
 
-public final class RemoteLoader: FeedLoader {
+public final class RemoteLoader<Resource> {
     private let url: URL
     private let client: HTTPClient
-    public typealias Result = FeedLoader.Result
+    private let mapper: Mapper
+    public typealias Result = Swift.Result<Resource, Swift.Error>
+    public typealias Mapper = (Data, HTTPURLResponse) throws -> Resource
     
     public enum Error: Swift.Error {
         case connectivity
         case invalidData
     }
     
-    public init(url: URL, client: HTTPClient) {
+    public init(url: URL, client: HTTPClient, mapper: @escaping Mapper) {
         self.url = url
         self.client = client
+        self.mapper = mapper
     }
     
-    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+    public func load(completion: @escaping (RemoteLoader.Result) -> Void) {
         client.get(from: url) { [weak self] result in
-            guard self != nil else { return }
+            guard let self = self else { return }
             switch result {
             case .success((let data, let response)):
-                completion(RemoteLoader.map(data, response))
+                completion(self.map(data, response))
             case .failure:
                 completion(.failure(RemoteLoader.Error.connectivity))
             }
         }
     }
     
-    private static func map(_ data: Data, _ response: HTTPURLResponse) -> Result {
+    private func map(_ data: Data, _ response: HTTPURLResponse) -> Result {
         do {
-            let items = try FeedItemsMapper.map(data, from: response)
+            let items = try mapper(data, response)
             return .success(items)
         } catch {
             return .failure(Error.invalidData)
