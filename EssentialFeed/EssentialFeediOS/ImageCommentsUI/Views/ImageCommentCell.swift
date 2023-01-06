62fe2c474a3779acbab6efb5acf7e649785484ec
Vitor Ferraz Varela <vitor.varela@bitso.com>
2023-01-06 10:11:34 -0300

    add comment list ui and snapshot tests

---
+++ EssentialFeed/EssentialFeediOS/ImageCommentsUI/Views/ImageCommentCell.swift
@@ -0,0 +1,15 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Vitor Ferraz Varela on 06/01/23.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet private(set) public var usernameLabel: UILabel!
+    @IBOutlet private(set) public var dateLabel: UILabel!
+    @IBOutlet private(set) public var messageLabel: UILabel!
+}
+
