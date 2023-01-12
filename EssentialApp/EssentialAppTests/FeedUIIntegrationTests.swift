d380e098adcc13bfe7ea559543b544dfa6c3c0a9
Vitor Ferraz Varela <vitor.varela@bitso.com>
2023-01-12 08:47:00 -0300

    Use diff able data source to reload cell only when its needed

--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -424,6 +424,11 @@ private extension UIRefreshControl {
 
 extension ListViewController {
     
+    public override func loadViewIfNeeded() {
+        super.loadViewIfNeeded()
+        tableView.frame = CGRect(x: 0, y: 0, width: 1, height: 1)
+    }
+    
     func simulateErrorViewTap() {
         errorView.simulateTap()
     }
@@ -440,6 +445,7 @@ extension ListViewController {
     }
     
     func numberOfRenderedFeedImageViews() -> Int {
+        tableView.numberOfSections == 0 ? 0 :
         tableView.numberOfRows(inSection: feedImagesSection)
     }
     
