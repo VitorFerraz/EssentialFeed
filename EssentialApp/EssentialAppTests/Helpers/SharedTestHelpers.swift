606189ef2b91a10a2dfd0324ae91f29d85ed997f
Vitor Ferraz Varela <vitor.varela@bitso.com>
2023-01-18 12:43:26 -0300

    make CommentsUIIntegrationTests sub class to XCTestCase instead of FeedUIIntegrationTests

--- EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/SharedTestHelpers.swift
@@ -41,3 +41,19 @@ func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
     }
     return (models, local)
 }
+
+class DummyView: ResourceView {
+    func display(_ viewModel: Any) {}
+}
+
+var loadError: String {
+    LoadResourcePresenter<Any, DummyView>.loadError
+}
+
+var feedTitle: String {
+    FeedPresenter.title
+}
+
+var commentsTitle: String {
+    ImageCommentsPresenter.title
+}
